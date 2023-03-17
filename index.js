b2ca3ab0a85784a89ff9c1dc9f911e933978d89e
Yababay <bigbelk@live.ru>
2023-03-17 12:20:08 +0300

    https

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import https from 'https'
 import express from 'express'
 import dotenv from 'dotenv'
+import { readFileSync } from 'fs'
 import router from './router/index.js'
 import basicAuth from 'express-basic-auth'
 
@@ -8,9 +10,9 @@ dotenv.config()
 const { HTTP_PORT, API_PATH, BASIC_AUTH } = process.env
 const [username, password] = (BASIC_AUTH || '').split(':')
 
-if(!/^\/[\w]/.test(API_PATH)) throw `Bad api path: ${API_PATH}`
+if(!/^\/[\w]/.test(API_PATH))  throw `Bad api path: ${API_PATH}`
 if(!/^[\d]+$/.test(HTTP_PORT)) throw `Bad http port: ${HTTP_PORT}`
-if(!(username && password)) throw `Basic auth is not set up.`
+if(!(username && password))    throw `Basic auth is not set up.`
 
 const app = express()
 const unauthorizedResponse = req => 'No credentials provided'
@@ -22,4 +24,12 @@ app.use((err, req, res, next) => {
     res.status(500).end(err.message)
 })
 
-app.listen(HTTP_PORT, () => console.log(`Listening on ${HTTP_PORT}.`))
+https.createServer({
+	key: readFileSync('./assets/domain.key'),
+	cert: readFileSync('./assets/domain.crt')
+}, app).listen(HTTP_PORT, () => {
+  console.log(`Listening ${HTTP_PORT}...`)
+});
+
+//app.listen(HTTP_PORT, () => console.log(`Listening on ${HTTP_PORT}.`))
+
